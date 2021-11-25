181b05b26688eb657ca88f518d9a0b9504a99218
Jordan Lange <langej11@gmatc.matc.edu>
2021-11-24 23:04:00 -0800

    completed activity 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,9 +11,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Domain;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
 using Persistence;
+using Application.Posts;
 
 namespace API
 {
@@ -24,11 +26,13 @@ namespace API
         }
 
         private readonly string CorsPolicy = "CorsPolicy";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddCors(opt =>
             {
                 opt.AddPolicy(CorsPolicy, policyBuilder =>
@@ -39,12 +43,14 @@ namespace API
                         .AllowAnyMethod();
                 });
             });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -56,12 +62,12 @@ namespace API
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                // app.UseHsts();
             }
 
-            //app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseCors(CorsPolicy);
-            app.UseMvc();
+            app.UseMvc()    ;
         }
     }
 }
